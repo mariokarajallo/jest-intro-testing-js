6cb2ff8b3c843bf864f5addbc7bd3d27eaf31356
mariokarajallo <mariokarajallo92@gmail.com>
2023-06-11 23:25:20 -0400

    feat: add test "actualizar citas"

--- __tests__/citas.js
+++ __tests__/citas.js
@@ -2,10 +2,11 @@ import Citas from "../js/Clases/Citas";
 
 //Instanciamos por fuera de los test para poder utilizar en varias pruebas
 const citas = new Citas();
-
+const id = Date.now();
 describe("Probar la clase de citas", () => {
   test("Agregar una nueva cita", () => {
     const citaObj = {
+      id,
       mascota: "Frodo",
       propietario: "Mario",
       telefono: "111222",
@@ -14,11 +15,24 @@ describe("Probar la clase de citas", () => {
       sintomas: "Duerme mucho",
     };
 
-    citaObj.id = Date.now();
-
     citas.agregarCita(citaObj);
 
     //prueba
+    expect(citas).toMatchSnapshot();
+  });
+
+  test("Actualizar cita", () => {
+    const citaActualizada = {
+      id,
+      mascota: "Kira",
+      propietario: "Mario",
+      telefono: "111222",
+      fecha: "11-06-23",
+      hora: "10:40",
+      sintomas: "Duerme mucho",
+    };
+
+    citas.editarCita(citaActualizada);
     expect(citas).toMatchSnapshot();
   });
 });
